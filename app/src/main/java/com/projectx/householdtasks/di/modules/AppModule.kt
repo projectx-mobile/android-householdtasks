92a29250d168fd331673163c260ca1339de5ef88
trokay <trokay70@gmail.com>
2022-09-20 01:01:29 +0300

    add navigation

--- app/src/main/java/com/projectx/householdtasks/di/modules/AppModule.kt
+++ app/src/main/java/com/projectx/householdtasks/di/modules/AppModule.kt
@@ -2,6 +2,7 @@ package com.projectx.householdtasks.di.modules
 
 import com.projectx.householdtasks.data.example.repository.ExampleRepository
 import com.projectx.householdtasks.presentation.viewmodel.MainViewModel
+import com.projectx.householdtasks.presentation.viewmodel.ParentHomescreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -9,4 +10,5 @@ val appModule = module {
     single { ExampleRepository(get()) }
 
     viewModel { MainViewModel() }
+    viewModel { ParentHomescreenViewModel() }
 }
--- app/src/main/java/com/projectx/householdtasks/presentation/MainActivity.kt
+++ app/src/main/java/com/projectx/householdtasks/presentation/MainActivity.kt
@@ -1,18 +1,9 @@
 package com.projectx.householdtasks.presentation
 
 
-import android.app.FragmentManager
-import android.app.FragmentTransaction
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.projectx.householdtasks.R
-import com.projectx.householdtasks.presentation.fragment.ParentHomescreenFragment
 
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 }
