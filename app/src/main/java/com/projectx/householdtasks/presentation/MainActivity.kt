89da19ea12675f6d95e7216a5ef186e687114869
trokay <trokay70@gmail.com>
2022-09-20 00:26:38 +0300

    improved ui

--- app/src/main/java/com/projectx/householdtasks/presentation/MainActivity.kt
+++ app/src/main/java/com/projectx/householdtasks/presentation/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.projectx.householdtasks.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.app.FragmentManager
+import android.app.FragmentTransaction
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.projectx.householdtasks.R
+import com.projectx.householdtasks.presentation.fragment.ParentHomescreenFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_parent_homescreen)
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/projectx/householdtasks/presentation/fragment/ParentHomescreenFragment.kt
@@ -0,0 +1,63 @@
+package com.projectx.householdtasks.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.projectx.householdtasks.databinding.FragmentParentHomescreenBinding
+import com.projectx.householdtasks.presentation.FamilyMembersListAdapter
+import com.projectx.householdtasks.presentation.UpdatesListAdapter
+import com.projectx.householdtasks.presentation.viewmodel.ParentHomescreenViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class ParentHomescreenFragment : BaseFragment() {
+
+    private var _binding: FragmentParentHomescreenBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModel<ParentHomescreenViewModel>()
+
+    private val customFamilyMembersAdapter = FamilyMembersListAdapter() {
+
+    }
+
+    private val customUpdatesAdapter = UpdatesListAdapter() {
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ) = FragmentParentHomescreenBinding.inflate(inflater, container, false).also {
+        _binding = it
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.bindUI().subscribeUI()
+    }
+
+    private fun FragmentParentHomescreenBinding.bindUI() = this.also {
+        customFamilyMembersAdapter.submitList(viewModel.getFamilyMembers())
+        customUpdatesAdapter.submitList(viewModel.getUpdates())
+
+        familyMembersRecyclerView.apply {
+            adapter = customFamilyMembersAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        updatesRecyclerView.apply {
+            adapter = customUpdatesAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+    }
+
+    private fun FragmentParentHomescreenBinding.subscribeUI() = this.also {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
