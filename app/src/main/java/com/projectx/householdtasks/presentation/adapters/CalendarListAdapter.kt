123bebc04eb2c97f62c9ac67e14744e9f6d19bf5
trokay <trokay70@gmail.com>
2022-10-05 00:20:04 +0300

    implemented calendar

--- app/src/main/java/com/projectx/householdtasks/presentation/adapters/CalendarListAdapter.kt
+++ app/src/main/java/com/projectx/householdtasks/presentation/adapters/CalendarListAdapter.kt
@@ -1,14 +1,20 @@
 package com.projectx.householdtasks.presentation.adapters
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.projectx.householdtasks.R
 import com.projectx.householdtasks.databinding.CalendarItemBinding
 import java.text.SimpleDateFormat
 
-class CalendarListAdapter(private val onItemClicked: (Long) -> Unit) :
+class CalendarListAdapter(
+    private val dayNames: Array<String>,
+    private val onItemClicked: (Long) -> Unit,
+) :
     ListAdapter<Pair<Long, Boolean>, CalendarListAdapter.CalendarHolder>(DiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarHolder {
@@ -18,28 +24,42 @@ class CalendarListAdapter(private val onItemClicked: (Long) -> Unit) :
                     parent.context
                 )
             ),
-            onItemClicked
+            onItemClicked,
+            dayNames
         )
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onBindViewHolder(holder: CalendarHolder, position: Int) {
         val current = getItem(position)
+
         holder.bind(current)
     }
 
     class CalendarHolder(
         private var binding: CalendarItemBinding,
         private val onItemClicked: (Long) -> Unit,
+        private val dayNames: Array<String>,
     ) :
         RecyclerView.ViewHolder(binding.root) {
 
+        @RequiresApi(Build.VERSION_CODES.M)
         fun bind(member: Pair<Long, Boolean>) {
             itemView.setOnClickListener {
                 onItemClicked(member.first)
             }
             binding.apply {
-                dayOfMonth.text = dateFormat.format(member.first)
-                dayOfMontNumber.text = member.second.toString()
+                dayNames[SimpleDateFormat("u").format(member.first).toInt() - 1].let {
+                    dayOfMonth.text = it
+                }
+                dayOfMontNumber.text = dateFormat.format(member.first)
+                if (member.second) {
+                    binding.cardView.setCardBackgroundColor(binding.root.context.getColor(R.color.task_new))
+
+                } else {
+                    binding.cardView.setCardBackgroundColor(binding.root.context.getColor(R.color.primary_white_snow))
+
+                }
             }
         }
     }
--- app/src/main/java/com/projectx/householdtasks/presentation/fragment/ChildHomescreenFragment.kt
+++ app/src/main/java/com/projectx/householdtasks/presentation/fragment/ChildHomescreenFragment.kt
@@ -1,39 +1,65 @@
 package com.projectx.householdtasks.presentation.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OVER_SCROLL_NEVER
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.projectx.householdtasks.R
 import com.projectx.householdtasks.databinding.FragmentHomescreenChildBinding
 import com.projectx.householdtasks.presentation.adapters.CalendarListAdapter
 import com.projectx.householdtasks.presentation.adapters.TaskListAdapter
 import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 class ChildHomescreenFragment : BaseFragment() {
 
     private var _binding: FragmentHomescreenChildBinding? = null
     private val binding get() = _binding!!
+
     //private val viewModel by viewModel<ChildHomescreenViewModel>()
+    private val weekDaysNames: Array<String> by lazy {
+        resources.getStringArray(R.array.week_days)
+    }
+    private val monthNames: Array<String> by lazy {
+        resources.getStringArray(R.array.month_names)
+    }
 
-    private val customCalendarListAdapter = CalendarListAdapter() { clickedDate ->
-        for (i in calendarList.indices) {
-            if (calendarList[i].first == clickedDate) {
-                calendarList[i] = Pair(calendarList[i].first, !calendarList[i].second)
-            } else {
-                calendarList[i] = Pair(calendarList[i].first, false)
+    private val customCalendarListAdapter by lazy {
+        CalendarListAdapter(weekDaysNames) { clickedDate ->
+            calendarList.forEachIndexed { index, pair ->
+                calendarList[index] = with(pair) {
+                    when (first) {
+                        clickedDate -> {
+                            selectedDate.timeInMillis = clickedDate
+                            Pair(first, true)
+
+                        }
+                        else -> Pair(first, false)
+                    }
+                }
             }
+            fd()
+            updateMonthTitle()
         }
-
     }
+
+    fun fd() {
+        customCalendarListAdapter.notifyDataSetChanged()
+    }
+
     private val customTaskListAdapter = TaskListAdapter() {
-
     }
-    var calendarList = mutableListOf<Pair<Long, Boolean>>()
-    val firstDayInCalendar = Calendar.getInstance()
-    val chosenDate = firstDayInCalendar
+
+    private var calendarList = mutableListOf<Pair<Long, Boolean>>()
+    private var firstDayInCalendar: Calendar = Calendar.getInstance()
+    private val selectedDate = Calendar.getInstance()
+    private val todaysDate = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,30 +75,15 @@ class ChildHomescreenFragment : BaseFragment() {
 
     private fun FragmentHomescreenChildBinding.bindUI() = this.also {
         updateCalendar()
-        val asfsaf = LinearLayoutManager(requireContext())
-        asfsaf.orientation = LinearLayoutManager.HORIZONTAL
-
+        toolbar.inflateMenu(R.menu.date_chooser)
         calendarRecyclerView.apply {
             adapter = customCalendarListAdapter
-            layoutManager = asfsaf
+            layoutManager = LinearLayoutManager(requireContext()).apply {
+                orientation = LinearLayoutManager.HORIZONTAL
+            }
         }
 
-        customTaskListAdapter.submitList(listOf(1,
-            2,
-            3,
-            1,
-            2,
-            3,
-            1,
-            2,
-            2,
-            3,
-            1,
-            2,
-            2,
-            3,
-            1,
-            2))
+        customTaskListAdapter.submitList(List(16) { (1..3).random() })
 
         taskListRecyclerView.apply {
             adapter = customTaskListAdapter
@@ -92,56 +103,113 @@ class ChildHomescreenFragment : BaseFragment() {
                     outRect.right = resources.getDimensionPixelSize(R.dimen.spacing_normal)
                 }
             })
+
+        flexibleExampleCollapsing.apply {
+            setExpandedTitleTextAppearance(R.style.ExpandedAppBar)
+            setCollapsedTitleTextAppearance(R.style.CollapsedAppBar)
+        }
+        val initToolbarHeight = flexibleExampleCollapsing.layoutParams.height
+        flexibleExampleCollapsing.layoutParams =
+            flexibleExampleCollapsing.layoutParams.apply { height = 0 }
+
         BottomSheetBehavior.from(coordinatorLayout).setBottomSheetCallback(
             object : BottomSheetBehavior.BottomSheetCallback() {
                 override fun onStateChanged(view: View, i: Int) {
                     when (i) {
                         BottomSheetBehavior.STATE_EXPANDED -> {
                             imageView234.visibility = View.GONE
-                            flexibleExampleAppbar.visibility = View.VISIBLE
-                        }
-                        BottomSheetBehavior.STATE_COLLAPSED -> {
-                            imageView234.visibility = View.VISIBLE
-                            flexibleExampleAppbar.visibility = View.GONE
-                        }
-                        BottomSheetBehavior.STATE_DRAGGING -> {
-                            imageView234.visibility = View.VISIBLE
-                            flexibleExampleAppbar.visibility = View.GONE
-                        }
-                        BottomSheetBehavior.STATE_HALF_EXPANDED -> {
-                            imageView234.visibility = View.VISIBLE
-                            flexibleExampleAppbar.visibility = View.GONE
                         }
                         else -> {
                             imageView234.visibility = View.VISIBLE
-                            flexibleExampleAppbar.visibility = View.GONE
                         }
                     }
                 }
 
-                override fun onSlide(view: View, v: Float) {}
+                override fun onSlide(view: View, v: Float) {
+                    val newHeight = (initToolbarHeight * v.absoluteValue).roundToInt()
+                    flexibleExampleCollapsing.layoutParams =
+                        flexibleExampleCollapsing.layoutParams.apply { height = newHeight }
+                }
             }
         )
+
+        taskListRecyclerView.overScrollMode = OVER_SCROLL_NEVER
+        calendarRecyclerView.overScrollMode = OVER_SCROLL_NEVER
     }
 
-
     private fun FragmentHomescreenChildBinding.subscribeUI() = this.also {
+        leftCalendar.setOnClickListener {
+            firstDayInCalendar.add(Calendar.DAY_OF_MONTH, -1)
+            updateCalendar()
+        }
+        rightCalendar.setOnClickListener {
+            firstDayInCalendar.add(Calendar.DAY_OF_MONTH, 1)
+            updateCalendar()
+        }
 
+        selectedDateTitle.setOnClickListener {
+            firstDayInCalendar.timeInMillis = todaysDate.timeInMillis
+            updateCalendar()
+        }
+
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_choose_date -> {
+                    val datePicker =
+                        MaterialDatePicker.Builder.datePicker()
+                            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                            .setTitleText("Select date")
+                            .setTheme(R.style.ThemeOverlay_App_DatePicker)
+                            .build()
+                            .show(requireActivity().supportFragmentManager, "tag");
+                    true
+                }
+                else -> false
+            }
+        }
+        calendarIcon.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setTitleText("Select date")
+                .setTheme(R.style.ThemeOverlay_App_DatePicker)
+                .build()
+                .let { materialDatePicker ->
+                    materialDatePicker.addOnPositiveButtonClickListener {
+                        firstDayInCalendar.timeInMillis = it
+                        selectedDate.timeInMillis = it
+                        updateCalendar()
+                    }
+                    materialDatePicker.show(requireActivity().supportFragmentManager, "tdsfag")
+                }
+        }
     }
 
     private fun updateCalendar() {
         calendarList.clear()
         repeat(5) {
-            if(firstDayInCalendar.timeInMillis == chosenDate.timeInMillis) {
+            Log.d("Calendar", "first: ${firstDayInCalendar.time} ---- second: ${selectedDate.time}")
+            if (firstDayInCalendar.timeInMillis == selectedDate.timeInMillis) {
                 calendarList.add(Pair(firstDayInCalendar.timeInMillis, true))
             } else {
                 calendarList.add(Pair(firstDayInCalendar.timeInMillis, false))
             }
             firstDayInCalendar.add(Calendar.DAY_OF_MONTH, 1)
         }
-
+        firstDayInCalendar.add(Calendar.DAY_OF_MONTH, -5)
         customCalendarListAdapter.submitList(calendarList)
         customCalendarListAdapter.notifyDataSetChanged()
+
+        updateMonthTitle()
+    }
+
+    private fun updateMonthTitle() {
+        binding.monthTitle.text = monthNames[selectedDate.get(Calendar.MONTH)]
+        binding.currentTasks.text = resources.getString(
+            R.string.active_tasks_format,
+            selectedDate.get(Calendar.DAY_OF_MONTH),
+            resources.getStringArray(R.array.month_names_current_tasks)[selectedDate.get(Calendar.MONTH)],
+            5
+        )
     }
 
     override fun onDestroy() {
