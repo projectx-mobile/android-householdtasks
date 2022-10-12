03565104ee6552c583245b6a51a971cd21ab10cd
Volha Mazurenka <58402090+VolhaMazurenka@users.noreply.github.com>
2022-10-12 18:49:43 +0200

    resolved merge conflicts

--- app/src/main/java/com/projectx/householdtasks/di/modules/AppModule.kt
+++ app/src/main/java/com/projectx/householdtasks/di/modules/AppModule.kt
@@ -3,7 +3,6 @@ package com.projectx.householdtasks.di.modules
 import com.projectx.householdtasks.data.example.repository.ExampleRepository
 import com.projectx.householdtasks.presentation.viewmodel.ChooseLoginTypeViewModel
 import com.projectx.householdtasks.presentation.viewmodel.LoginViewModel
-import com.projectx.householdtasks.presentation.viewmodel.MainViewModel
 import com.projectx.householdtasks.presentation.viewmodel.ParentHomescreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -13,6 +12,5 @@ val appModule = module {
 
     viewModel { ChooseLoginTypeViewModel() }
     viewModel { LoginViewModel() }
-    viewModel { MainViewModel() }
     viewModel { ParentHomescreenViewModel() }
 }
